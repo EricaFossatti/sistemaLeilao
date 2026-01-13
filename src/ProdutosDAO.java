d87e30df14038fa5ecba4ef62e58b9e5f06a96de
EricaFossatti <ericafossatti06@gmail.com>
2026-01-13 16:47:21 -0300

    Commit #3: listagem de produtos implementada

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -44,7 +45,22 @@ public class ProdutosDAO {
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        String sql = "SELECT * FROM produtos";
+        conn = new conectaDAO().connectDB();
+        try{
+            prep= conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+            listagem.add(produto);
+            }
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
+        }
         return listagem;
     }
     
